f4d319c47c8e620569db0b1d8aeec670e779e8e3
RazumeetsyaDanila <danil.kozhemyakin.99@mail.ru>
2020-10-20 16:47:06 +0300

    Updates

---
+++ precache-manifest.0e963cb716804767fb20938c849e9bcc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4308f6fbcea3ee5c284c9c6600bedaf",
+    "url": "/labWorks/index.html"
+  },
+  {
+    "revision": "7f02aadbaf7b9d16c480",
+    "url": "/labWorks/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f9a87db2b1ab440f0282",
+    "url": "/labWorks/static/js/2.15548f98.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/labWorks/static/js/2.15548f98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f02aadbaf7b9d16c480",
+    "url": "/labWorks/static/js/main.fc718fac.chunk.js"
+  },
+  {
+    "revision": "96e677e2ddb172d867cf",
+    "url": "/labWorks/static/js/runtime-main.f4747c78.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/labWorks/static/media/logo.5d5d9eef.svg"
+  }
+]);
